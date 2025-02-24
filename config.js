45cb98c35d5396a7712817cfe7e121d887d750f2
jajamiekun <jajamiekun@gmail.com>
2025-02-24 12:50:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,11 @@
 module.exports = [
     {
-        usertoken: 'usertoken1',
+        usertoken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2Nzk2NjM4YWFhNTI5MDZjNzRhNTA5YzIiLCJwdWJsaWNBZGRyZXNzIjoiQzltaVNvbnBMaEJ6TnN6UFFYZzVndEhCRU1ocVlVeFlwdjRiaGZWalBHUSIsIndhbGxldFR5cGUiOiJzb2xhbmEiLCJpYXQiOjE3Mzg0NjQ1MDksImV4cCI6MTc3MDAyMjEwOX0.PEcTruWHge77WMpTfa7TGdBgrd1vOnggxUT-LHMpcfk',
         nodes: [
-            { nodeId: 'nodeid1', hardwareId: 'hardwareid1', proxy: 'proxy1' },
-            { nodeId: 'nodeid2', hardwareId: 'hardwareid2', proxy: 'proxy2' },
-            { nodeId: 'nodeid3', hardwareId: 'hardwareid3', proxy: 'proxy3' },
-            { nodeId: 'nodeid4', hardwareId: 'hardwareid4', proxy: 'proxy4' },
-            { nodeId: 'nodeid5', hardwareId: 'hardwareid5', proxy: 'proxy5' }
-        ]
-    },
-    {
-        usertoken: 'usertoken2',
-        nodes: [
-            { nodeId: 'nodeid6', hardwareId: 'hardwareid6', proxy: 'proxy6' },
-            { nodeId: 'nodeid7', hardwareId: 'hardwareid7', proxy: 'proxy7' },
-            { nodeId: 'nodeid8', hardwareId: 'hardwareid8', proxy: 'proxy8' },
-            { nodeId: 'nodeid9', hardwareId: 'hardwareid9', proxy: 'proxy9' },
-            { nodeId: 'nodeid10', hardwareId: 'hardwareid10', proxy: 'proxy10' }
-        ]
+            { nodeId: '12D3KooWKikAvPQqFZg9xmcQ2WQhDRMeVw8xiKfFSs4r4HDcvuus', hardwareId: '77cfa600a14021599cadadb2734318ce0252fe8a9d06f2ded41dd6a3a5f814fc', proxy: 'http://pymiivur:o5tci5ctvvde@69.91.142.119:7611' },
+            { nodeId: '12D3KooWQjXXPMbUHm3UCZSo3Z7bJMu9pSGrhep4uAH54eVKmXGA', hardwareId: '9fa42f932cfa63fce3b24ef64174a82fc35f433fb5ed48d91ed62255a3094eee', proxy: 'http://pymiivur:o5tci5ctvvde@156.238.176.57:6739' },
+            { nodeId: '12D3KooWHuPtMWVtSf74RFpmqWZZEYrerRPg2m8VDuhNuoqqvCCT', hardwareId: '50ca911d1ca224e74d37aae9c14efbb21d2572577f2f0789df18164f9533be92', proxy: 'http://pymiivur:o5tci5ctvvde@63.141.58.133:6449' }
+		]
     }
     // Add more usertokens as needed
 ];
