f3b4b65e47371acc0d951d2f2ed78ef401d3a763
jajamiekun <jajamiekun@gmail.com>
2025-03-01 09:35:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ module.exports = [
     {
         usertoken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2Nzk2NjM4YWFhNTI5MDZjNzRhNTA5YzIiLCJwdWJsaWNBZGRyZXNzIjoiQzltaVNvbnBMaEJ6TnN6UFFYZzVndEhCRU1ocVlVeFlwdjRiaGZWalBHUSIsIndhbGxldFR5cGUiOiJzb2xhbmEiLCJpYXQiOjE3Mzg0NjQ1MDksImV4cCI6MTc3MDAyMjEwOX0.PEcTruWHge77WMpTfa7TGdBgrd1vOnggxUT-LHMpcfk',
         nodes: [
-            { nodeId: '12D3KooWKikAvPQqFZg9xmcQ2WQhDRMeVw8xiKfFSs4r4HDcvuus', hardwareId: '77cfa600a14021599cadadb2734318ce0252fe8a9d06f2ded41dd6a3a5f814fc', proxy: 'http://pymiivur:o5tci5ctvvde@69.91.142.119:7611' },
-            { nodeId: '12D3KooWQjXXPMbUHm3UCZSo3Z7bJMu9pSGrhep4uAH54eVKmXGA', hardwareId: '9fa42f932cfa63fce3b24ef64174a82fc35f433fb5ed48d91ed62255a3094eee', proxy: 'http://pymiivur:o5tci5ctvvde@156.238.176.57:6739' },
-            { nodeId: '12D3KooWHuPtMWVtSf74RFpmqWZZEYrerRPg2m8VDuhNuoqqvCCT', hardwareId: '50ca911d1ca224e74d37aae9c14efbb21d2572577f2f0789df18164f9533be92', proxy: 'http://pymiivur:o5tci5ctvvde@63.141.58.133:6449' }
+            { nodeId: '12D3KooWKikAvPQqFZg9xmcQ2WQhDRMeVw8xiKfFSs4r4HDcvuus', hardwareId: '77cfa600a14021599cadadb2734318ce0252fe8a9d06f2ded41dd6a3a5f814fc', proxy: 'http://pymiivur:o5tci5ctvvde@130.180.233.10:7581' },
+            { nodeId: '12D3KooWQjXXPMbUHm3UCZSo3Z7bJMu9pSGrhep4uAH54eVKmXGA', hardwareId: '9fa42f932cfa63fce3b24ef64174a82fc35f433fb5ed48d91ed62255a3094eee', proxy: 'http://pymiivur:o5tci5ctvvde@156.237.19.208:5605' },
+            { nodeId: '12D3KooWHuPtMWVtSf74RFpmqWZZEYrerRPg2m8VDuhNuoqqvCCT', hardwareId: '50ca911d1ca224e74d37aae9c14efbb21d2572577f2f0789df18164f9533be92', proxy: 'http://pymiivur:o5tci5ctvvde@154.194.16.252:6171' }
 		]
     }
     // Add more usertokens as needed
